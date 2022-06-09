29039b9ec9baceb299a22392bf90e071f4775a72
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-09 22:26:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from discord import Client
-from sdk import connect
+import discord
+from sdk import Client as SdkClient
+from orjson import load
 
 
-class MyClient(Client):
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        with open("config.json", "w") as f:
+            self.config = load(f)
+        self.sdk = SdkClient()
+    
     async def setup_hook(self):
-        self.ugc = await connect(
+        self.loop.create_task(self.sdk.connect())
+        
+    def on(self, name):
+        return self.sdk.on(name)
+    
+client = MyClient(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("ready")
+    
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.channel.name != "ugc-test":
+        return
+    for ch in client.channels:
+        if ch.name == "ugc-test":
+            embed = discord.Embed(description=message.content)
+            embed.set_author(name=message.author.name, url=getattr(message.author.avatar, "url", None))
+            await ch.send(embed=embed)
+
+client.run(self.config["token"])
