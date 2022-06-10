049c50fc003ea575fc417b4410f3e1161988836d
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-10 22:05:57 +0900

    Update items.py

--- sdk/items.py
+++ sdk/items.py
@@ -1,6 +1,9 @@
 class Message:
     def __init__(self, data: dict):
         self.channel = Channel(data["channel"])
+        self.author = User(data["author"])
+        self.guild = Guild(data["guild"])
+        self.content = data["message"]["content"]
         
     
 class Channel:
@@ -12,3 +15,12 @@ class User:
     def __init__(self, data: dict):
         self.name = data["username"]
         self.id = data["id"]
+        self.discriminator = data["discriminator"]
+        self.avatar_url = data["avatarURL"]
+        self.bot = data["bot"]
+        
+class Guild:
+    def __init__(self, data: dict):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.iconURL = data["iconURL"]
