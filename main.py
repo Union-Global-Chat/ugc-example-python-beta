1c2f2adc2845d75e94076b6a0db176e4c8d41980
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-11 15:14:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def on_ready():
     
 @tree.command(description="Pong")
 async def ping(interaction):
-    await interaction.response.send_message(client.sdk.latency)
+    await interaction.response.send_message(round(client.sdk.latency * 1000))
 
 
 @client.on("ready")
